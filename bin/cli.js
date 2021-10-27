73670e6be58497a3f99d9c05eccc15abf44a7cdb
bihtyu <bihtyu@163.com>
2021-10-27 08:25:55 +0800

    update lib

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
-
 const program = require('commander')
+const chalk = require('chalk')
+const figlet = require('figlet')
 
 program
   // 定义命令和参数
@@ -9,14 +10,25 @@ program
   // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
   .option('-f --force', 'voerwrite target directory if it exist')
   .action((name, options) => {
-    // 打印执行结果
-    console.log(`name: ${name}, options: ${options}`)
+    require('../lib/create')(name, options)
   })
 
 program
   // 配置版本信息
   .version(`v${require('../package.json').version}`)
   .usage('<command> [option]')
+
+program
+  .on('--help', () => {
+    console.log('\r\n' + figlet.textSync('gov cli', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    }))
+    console.log(`\r\nRun ${chalk.cyan(`gov <command> --help`)} for detailed usage of given command\r\n`)
+  })
 
 // 解析用户执行命令传入参数
 program.parse(process.argv)
